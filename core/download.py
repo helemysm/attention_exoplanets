710e553717336de6274f285112798ba7d6334ecc
helem.ysm <yes.helem@gmail.com>
2023-03-22 14:02:48 -0300

    Update download.py

--- core/download.py
+++ core/download.py
@@ -8,11 +8,11 @@ from tqdm import tqdm
 ## build the sh file for downloading the light curves
 def download_lc(catalog, output ):
     
-    FLAGS_output_file = 'tess_lcs.sh'
+    output_file = 'tess_lcs.sh'
     _wget_ = 'curl -C - -L -o '
     url_ = 'https://mast.stsci.edu/api/v0.1/Download/file/?uri=mast:TESS/product/'
 
-    with open(FLAGS_output_file, "w") as f:
+    with open(output_file, "w") as f:
             f.write("#!/bin/sh\n")
             for name_file in len(files.values):
 
