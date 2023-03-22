b7d64d8fb3371ba9537d442db642ba7535d7f9c1
helem.ysm <yes.helem@gmail.com>
2023-03-22 13:51:27 -0300

    Add files via upload

---
+++ core/download.py
@@ -0,0 +1,29 @@
+import os, sys
+import pandas as pd
+import numpy as np
+from tqdm import tqdm
+
+
+
+
+def download_lc(catalog, output ):
+    
+    FLAGS_output_file = 'get_tess_exo.sh'
+    _wget_ = 'curl -C - -L -o '
+    url_ = 'https://mast.stsci.edu/api/v0.1/Download/file/?uri=mast:TESS/product/'
+
+    with open(FLAGS_output_file, "w") as f:
+            f.write("#!/bin/sh\n")
+            for name_file in len(files.values):
+
+                f.write("{}{} {}{}\n".format(_wget_, name_file[2], url_, name_file[2]))
+
+            f.write("echo 'Finish {} Tess targets to {}'\n".format(len(files.values), 'TESS/'))
+
+
+catalog = 'exofop_tess_tois.csv' 
+output = 'tess_data_lc/'
+
+files = pd.read_csv(catalog)
+
+download_lc(catalog, output)
